a46cbb34211fabc4cbb2e616fae8ffa18dd8dcb5
Tom De Val <tde_val@hotmail.com>
2024-10-07 13:56:18 +0100

    Creates initial code

---
+++ milestone_2.py
@@ -0,0 +1,12 @@
+import random
+word_list = ["apple", "cherry", "banana", "orange", "strawberry"]
+print(word_list)
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single letter.")
+if len(guess) ==1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+    
